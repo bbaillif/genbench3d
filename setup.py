6aa0ced3ba2a23cdb0e312cc8fc9b172ea9acab7
bbaillif <benoit.baillif@gmail.com>
2023-09-20 15:22:26 +0100

    add setup.py for dev package install

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='genbench3d',
+    version='0.1.0',    
+    description='Benchmarking 3D molecular generative models',
+    url='https://github.com/bbaillif/genbench3d',
+    author='Benoit Baillif',
+    author_email='bb596@cam.ac.uk',
+    license='MIT license',
+    packages=['genbench3d'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.11',
+    ],
+)
